cba2f38a806d9336d52e489bab40b00d2e3d8715
Rayyan Khan <rayyan.khan258@gmail.com>
2019-03-12 11:28:52 -0400

    oops add regex 4

--- regex5.py
+++ regex5.py
@@ -14,29 +14,29 @@ import sys
 inp = int(sys.argv[1]) - 71 if len(sys.argv) > 1 else 0
 
 answerArray = [ # 71. Find the shortest word where some letter occurs 4 times.
-                '"/(?=\\b\w*(\w)(\w*\\1\w*){3}\\b)\w{,6}$/m"',
+                '/(?=\\b\w*(\w)(\w*\\1\w*){3}\\b)\w{,6}$/im',
 
                 # 72. Find the shortest word where each vowel appears at least once.
-                '',
+                '/\\b(?=\w*a)(?=\w*e)(?=\w*i)(?=\w*o)(?=\w*u)\w{,7}$/im',
 
-                # 73. Find the longest word containing exactly 3 vowels.
-                '/(?=\\b(\w*[aieou]\w*){3}\\b)\w{,3}$/m',
+                # 73. Find the longest word containing exactly 5 vowels.
+                '/^(?=\w{18,})([b-d,f-h,j-n,p-t,v-z]*[aeiou]){5}[b-d,f-h,j-n,p-t,v-z]*$/mi',
 
                 # 74. Find the longest word where the first three letters form a
                 # palindrome with the final three letters.
-                '',
+                '/(?=\w*(.)(.)(.)$)^\\3\\2\\1\w{8,}/im',
 
                 # 75. Find the longest word with the longest contiguous block of
                 # one letter.
-                '/(?=\\b\w*(\w)\w*\\1{3,}\w*\\b)\w*$/m',
+                '/(?=\\b\w*(\w)\\1{1,}\w*\\b)\w{18,}$/m',
 
                 # 76. Find the longest word with the greatest number of a repeated
                 # letters.
-                '',
+                '/\w*(\w)(\w*\\1){5,}\w*$/im',
 
                 # 77. Find the longest word with the most number of adjacent pairs
-                # of identical letters. Mississippi?
-                '',
+                # of identical letters.
+                '/(?=\w{13,})(\w*?(\w)\\2){3,}\w*$/im',
 
                 # 78. Find the longest word where each letter is repeated at least
                 # once.
@@ -48,7 +48,7 @@ answerArray = [ # 71. Find the shortest word where some letter occurs 4 times.
 
                 # 80. Find the longest word where no letter is repeated more than
                 # once.
-                ''
+                '/^(?:(\w)(?!.*\\1.*\\1)){18,}$/im'
 ]
 
 print(answerArray[inp])
